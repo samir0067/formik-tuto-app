4fd38f43bf69849f3a6f72a04357d51b6b235450
samir0067 <arrahmani.samir@gmail.com>
2020-11-10 21:04:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Styles } from "./Styles"
+import {Form, Formik, useField} from "formik"
+import * as Yup from 'yup'
+
+const CustomeTextInput = ({label, ...props}) => {
+  const [field, meta] = useField(props)
+
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
+
+const CustomCheckox = ({children, ...props}) => {
+  const [field, meta] = useField(props, 'checkbox')
+
+  return (
+    <>
+      <label className="checkbox">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
+
+const CustomSelect = ({label, ...props}) => {
+  const [field, meta] = useField(props)
+
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Styles>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          acceptedTerms: false,
+          specialPower: '',
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string()
+            .min(3, 'Must be at least 3 characters')
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+          acceptedTerms: Yup.boolean()
+            .oneOf([true], 'You must accept the terms and conditions')
+            .required('Required'),
+          specialPower: Yup.string()
+            .oneOf(['flight', 'invisibility', 'wealthy bat guy', 'other'], 'Invalid Special Power')
+            .required('Required'),
+        })}
+        onSubmit={(values, {setSubmitting, resetForm}) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            resetForm()
+            setSubmitting(false)
+          }, 3000)
+        }}
+      >
+        {props => (
+          <Form>
+            <h1>Sign Up</h1>
+            <CustomeTextInput label="Name" name="name" type="text" placeholder="Samir"/>
+            <CustomeTextInput label="Email" name="email" type="email" placeholder="samir@email.com"/>
+            <CustomSelect label="Special Power" name="specialPower">
+              <option value="">Select a Special Power</option>
+              <option value="flight">Flight</option>
+              <option value="invisibility">Invisibility</option>
+              <option value="wealthy bat guy">wealthy bat guy</option>
+              <option value="other">Other</option>
+            </CustomSelect>
+            <CustomCheckox name="acceptedTerms">
+              I accept the terms and conditions
+            </CustomCheckox>
+            <button type="submit">{props.isSubmitting ? "Loading..." : "Submit"}</button>
+          </Form>
+        )}
+      </Formik>
+    </Styles>
+  )
 }
 
-export default App;
+export default App
